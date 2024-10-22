6f9118335593eb3868b991df376fc2a82e585745
cheryl9012 <161365360+cheryl9012@users.noreply.github.com>
2024-10-22 23:32:30 +0500

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import TodoApp from "./components/Todo";
+import "./App.css"
+
+
+export default function App(){
+    return(
+    <div className="App">
+        <h1> My Todo List App</h1>
+        <TodoApp />
+
+    </div>)
+}
